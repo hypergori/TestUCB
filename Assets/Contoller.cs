eaf830b2df2e37e1ef7020efa7f3c3361b8778d9
Masashi Nakane <hypergori@gmail.com>
2015-09-06 18:22:53 +0900

    Initial Commit

---
+++ Assets/Contoller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Contoller : MonoBehaviour {
+
+	public Rigidbody rb ; 
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+	
+		rb.AddForce (new Vector3 (h, 0, v) * 10);
+	}
+}
